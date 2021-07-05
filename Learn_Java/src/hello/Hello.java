7c78511b28fd284c5c47819e05f84781eb1b83d5
Courtney Alexander (ZA) <Courtney.Alexander@absa.africa>
2021-07-05 12:04:24 +0200

    Eliza

--- Learn_Java/src/hello/Hello.java
+++ Learn_Java/src/hello/Hello.java
@@ -1,9 +1,8 @@
-/**
- * Hello
- */
+package src.hello;
+
 public class Hello {
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
